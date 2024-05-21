567c37c5f7dd7a409ef40f796ca0817b92b03815
EmanuelVidal <emanuelnvidal94@gmail.com>
2024-05-21 00:14:10 -0300

    intente hacer el formulario y su validacion pero no pude hacerlo funcionar. Segui paso a paso como el profe dijo pero no hubo caso

---
+++ static/js/Contact.js
@@ -0,0 +1,34 @@
+const formRegister = document.querySelector("#form-register");
+
+const validarFormulario = (event) => {
+    event.preventDefault();//me permite hacer una validacion personalizada
+
+    const firstname = document.querySelector("#firstname");
+    const lastname = document.querySelector("#lastname");
+    const email = document.querySelector("#email");
+    let validation = true;
+
+    if(firstname.value.trim() ===""){ //elimino los espacios en blanco con trim
+        // alert("El campo nombre no debe estar vacio") (una forma de dar aviso)
+        document.querySelector("#error-firstname").textContent = "Debe completar el nombre"; //otra forma de dar el alerta
+        validation = false;
+    }
+    if(lastname.value.trim() ===""){ 
+        // alert("El campo nombre no debe estar vacio")
+        document.querySelector("#error-lastname").textContent = "Debe completar el apellido"; 
+        validation = false;
+    }
+    if(email.value.trim() ===""){ 
+        // alert("El campo nombre no debe estar vacio")
+        document.querySelector("#error-email").textContent = "Debe completar el campo de email"; 
+        validation = false;
+    }
+
+    if(validation){
+        formRegister.submit()
+    } else{
+        return false;
+    }//si es true se envia el formulario
+}
+
+formRegister.addEventListener("submit", validarFormulario);
